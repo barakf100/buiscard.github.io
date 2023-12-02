91b57f13dd7f0975453dd65f4ff95b9a1694b630
barak froimovich <barakf100@gmail.com>
2023-12-02 16:38:39 +0200

    build

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import axios from "axios";
 import store from "./store/bigPie";
 import { Provider } from "react-redux";
@@ -30,9 +30,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
+            {/* </BrowserRouter> */}
         </Provider>
     </React.StrictMode>
 );
